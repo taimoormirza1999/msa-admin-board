d23ef06bedbc2fe4e48895dc1dd0bcd6b8335e83
Taimoor Hamza <taimoorhamza1999@gmail.com>
2025-01-30 19:51:57 +0400

    update

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,15 +1,17 @@
 // import { metadata } from './page';
+import React from 'react';
 
 export const metadata = {
   title: "Next.js Settings | TailAdmin - Next.js Dashboard Template",
   description:
     "This is Next.js Settings page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
+interface SettingsLayoutProps {
+    children: React.ReactNode; // Explicitly define the type for children
+  }
 
-export default function SettingsLayout({ children }) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
-}
+  const SettingsLayout: React.FC<SettingsLayoutProps> = ({ children }) => {
+    return <div>{children}</div>;
+  };
+  
+  export default SettingsLayout;
